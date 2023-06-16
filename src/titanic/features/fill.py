83f7440f8f18c8ac34528c85572f441ef2762c26
bogorodz <bogorodz@gmail.com>
2023-06-16 17:21:18 +0300

    add fillna age

--- src/titanic/features/fill.py
+++ src/titanic/features/fill.py
@@ -1,8 +1,9 @@
 import pandas
+import numpy as np
 from sklearn.impute import SimpleImputer
 
 
-__all__ = ["embarked_imputer", "fill_embarked"]
+__all__ = ["embarked_imputer", "fill_embarked", "fill_age"]
 
 
 def embarked_imputer() -> SimpleImputer:
@@ -11,3 +12,14 @@ def embarked_imputer() -> SimpleImputer:
 
 def fill_embarked(df: pandas.DataFrame) -> pandas.Series:
     return embarked_imputer().fit_transform(df[["Embarked"]])
+    
+    
+def fill_age(data: pandas.DataFrame) -> pandas.DataFrame:
+    
+    columns = ['Pclass','Sex','Embarked','is_alone']
+    return pandas.Series(
+        pandas.merge(data.loc[:, columns],
+                 data.groupby(columns)['Age'].count().reset_index(), 
+                 on=columns, how='left')['Age'], 
+        index=data.index).replace(np.nan, data.loc[:, 'Age'].mean())
+
